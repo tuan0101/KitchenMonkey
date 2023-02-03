2fa3ca2d9c3b05edee4823e9d61a737e07408bd8
tuan0101 <tuan.programmer0101@gmail.com>
2023-02-02 22:07:36 -0800

    Interact with Counters

--- Assets/_Scripts/GameInput/GameInput.cs
+++ Assets/_Scripts/GameInput/GameInput.cs
@@ -1,16 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    public event EventHandler OnInteractAction;
     PlayerInputActions playerInputActions;
 
     private void Awake()
     {
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
+
+        // subscribe to this event
+        // instead of check if key is press in Update()
+        playerInputActions.Player.Interact.performed += Interact_performed;         
     }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnInteractAction?.Invoke(this, EventArgs.Empty);
+    }
+
     public Vector2 GetmovementVector()
     {
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
--- Assets/_Scripts/PLayer/Player.cs
+++ Assets/_Scripts/PLayer/Player.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    public class OnSelectedCounterChangedEventArgs : EventArgs
+    {
+        public ClearCounter _selectedCounter;
+    }
+
     [SerializeField] float moveSPeed = 7f;
     [SerializeField] GameInput gameInput;
-    bool isWalking;
+    [SerializeField] LayerMask counterLayerMask;
 
+    bool isWalking;
+    Vector3 lastInteractDir;
+    ClearCounter selectedCounter;
+
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
+    {
+        if (selectedCounter != null)
+        {
+            selectedCounter.Interact();
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
         HandleMovement();
+        HandleInteraction();
     }
 
     public bool IsWalking()
@@ -31,5 +55,38 @@ public class Player : MonoBehaviour
 
         float rotateSpeed = 10f;
         transform.forward = Vector3.Lerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    void HandleInteraction()
+    {
+        Vector2 inputVector = gameInput.GetmovementVector();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDir != Vector3.zero) lastInteractDir = moveDir;
+
+        float interactDistance = 2f;
+        if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, counterLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                if (clearCounter != selectedCounter)
+                {
+                    SetSelectedCounter(clearCounter);
+                }
+            }
+            else
+                SetSelectedCounter(null);
+        }
+        else
+            SetSelectedCounter(null);
+    }
+
+    void SetSelectedCounter(ClearCounter selectedCounter)
+    {
+        this.selectedCounter = selectedCounter;
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs
+        {
+            _selectedCounter = selectedCounter
+        });
     }
 }
